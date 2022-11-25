e6ab857eb5d11c27e1d3c91e82514e2e6f7816b0
Youssef434 <fatmi2019py@gmail.com>
2022-11-25 09:18:42 +0100

    second commit

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-app.use('/', express.static(path.join(__dirname, 'movies-explorer')))
+app.use('/', express.static(path.join('', 'movies-explorer')))
 
-app.get('', (req, res) => {
-  res.sendFile(path.join(__dirname, 'movies-explorer', 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join('', 'movies-explorer', 'index.html'));
 })
 
 app.listen(port, () => {
